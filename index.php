a8f44ee56abce4f162f4bbbb132e9c20a7a6a0c7
Leonie Cruz <leonie.cruz.10@gmail.com>
2024-05-15 19:16:34 +0200

    premier commit pour mamie leo

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Les recettes de Mamie Léo</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <img src="" alt="Logo">
+            <ul>
+                <li>Accueil</li>
+                <li>Recettes</li>
+                <li>Souvenirs</li>
+            </ul>
+        </nav>
+        <h1>Les recettes de Mamie Léo</h1>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Facere tenetur, adipisci, temporibus quam totam velit cumque earum deserunt optio aliquid voluptas, explicabo reprehenderit iusto enim numquam exercitationem. Praesentium, enim. Vero.
+        Facere ducimus quaerat dolores delectus quas fugit harum a mollitia, dolore id doloremque, nesciunt alias, ab vel beatae non est exercitationem! Placeat optio sapiente vero facilis inventore rem blanditiis consequuntur?
+        Blanditiis dicta, sit rerum explicabo amet culpa numquam tempore! Sequi quisquam eaque debitis nemo asperiores nobis unde quasi ut sapiente, modi nulla? Consequatur possimus unde eos incidunt natus nam pariatur.
+        Quidem consequatur nobis nostrum nihil dolores ex hic explicabo officiis necessitatibus magni porro assumenda adipisci, corrupti tempora aliquid soluta ducimus eligendi quod pariatur! Id deleniti possimus, cum veritatis quidem enim!
+        Hic recusandae dolores dolorem aliquid? Veniam numquam sint nesciunt nam amet? Pariatur modi praesentium nihil maxime. Quia, accusantium cupiditate. Eligendi nisi culpa labore doloribus, necessitatibus ex eius ea ullam! Veniam!
+        Voluptas velit veniam nemo explicabo voluptatem quis minus suscipit voluptates quo, officiis tenetur, ab, aliquid temporibus incidunt atque iusto accusamus? Eveniet magni tempora exercitationem ex laborum recusandae ducimus molestiae ad.
+        Earum labore exercitationem numquam voluptatum quasi. Voluptates ratione dolorem repellat perspiciatis recusandae, vitae quod corporis quas facere asperiores eos aliquam esse iusto commodi delectus illo magni vero ad dolore. Eaque.
+        Minus, dolores tempore repellendus nostrum, quod laborum est, placeat voluptate incidunt sint doloremque eum cumque explicabo eius corrupti ducimus ad eos consequuntur provident. Accusantium eos, voluptates ea eveniet animi dicta?
+        Sunt adipisci autem saepe quasi quam nam totam qui, ab aperiam quidem fuga vitae minima! Tenetur eius excepturi asperiores ipsa qui laboriosam in. Minus facere, illo incidunt id autem ab.</p>
+    </header>
+</body>
+</html>
