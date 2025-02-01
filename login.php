3f2c00936dab9172820a6b10ecf26251620c4e04
Leonie Cruz <leonie.cruz.10@gmail.com>
2025-02-01 16:40:37 +0100

    Fontionne mais style KC

--- login.php
+++ login.php
@@ -18,7 +18,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $_SESSION['user_id'] = $user['id'];
             $_SESSION['username'] = $user['username'];
             $_SESSION['role'] = $user['role'];
-            header("Location: souvenirs.php");
+            header("Location: index.php");
             exit;
         } else {
             // Connexion échouée
--- recipe.php
+++ recipe.php
@@ -4,19 +4,6 @@ session_start();
 require_once './_includes/header.php';
 require_once './config/Database.php';
 
-
-error_log("Recipe.php started");
-error_log("REQUEST_URI: " . $_SERVER['REQUEST_URI']);
-error_log("GET parameters: " . json_encode($_GET));
-
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-echo "URL complète : " . $_SERVER['REQUEST_URI'] . "<br>";
-echo "Contenu de \$_GET : ";
-var_dump($_GET);
-echo "<br>";
-
-
 // Vérifiez si l'utilisateur est connecté
 if (!isset($_SESSION['user_id'])) {
     echo "Vous devez être connecté pour accéder à cette page.";
@@ -49,16 +36,7 @@ try {
     $stmt->bindParam(':id', $id, PDO::PARAM_INT);
     $stmt->execute();
 
-    // Débogage
-    // echo "Requête SQL : " . $stmt->queryString . "<br>";
-    // echo "Paramètres : ";
-    // $stmt->debugDumpParams();
-    // echo "<br>";
-
     $recipe = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    // Débogage
-    // var_dump($recipe);
 
     if (!$recipe) {
         echo "<p>Recette introuvable.</p>";
